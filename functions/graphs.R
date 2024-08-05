c0789c9af7ec286164e96a523f20d33b24fd50ab
Ewen Gallic <ewen.gallic@gmail.com>
2024-08-05 11:35:23 -0400

    Descriptions for parameters

--- functions/graphs.R
+++ functions/graphs.R
@@ -1,97 +1,127 @@
-# helper functions to convert between radians and degrees
-deg2rad <- function (deg){
+#' Helper functions to convert from degree to radian
+#'
+#' @param deg vector of numeric in degree
+deg2rad <- function (deg) {
   stopifnot(is.numeric(deg))
-  (rad <- (pi/180) * deg)
+  (rad <- (pi / 180) * deg)
 }
 
-rad2deg <- function (rad){
+#' Helper functions to convert from radian to degree
+#'
+#' @param rad vector of numeric in degree
+rad2deg <- function (rad) {
   stopifnot(is.numeric(rad))
-  (deg <- rad/(pi/180))
+  (deg <- rad / (pi / 180))
 }
 
-# function to calculate the points on an arc between two points
-# position 1: x0, y0
-# position 2: x1, y1
-# number of points in resulting arc line: n
-# degrees of the arc connecting the points (positive is counter-clockwise, 
-# negative is clockwise): arcdeg
-calcArc <- function(x0 = 1, y0 = 1, x1 = 4, y1 = 6, arcdeg = 30, n = 50){
-  
-  if(abs(arcdeg)>-359.9 & abs(arcdeg)>359.9){stop("angle of arc (arcdeg) must be between -359.9 and 359.9")}
-  
-  anglerad <- atan2(y = y1-y0, x = x1-x0) # angle between points
-  midpt <- list(x = mean(c(x0,x1)), y = mean(c(y0,y1))) # midpoint coordinates of chord
-  arcrad <- deg2rad(deg = arcdeg) # angle of arc in radians
-  chordlength <- sqrt((x1-x0)^2 + (y1-y0)^2) # length between points
-  r <- abs((chordlength/2) / sin(arcrad/2)) # radius of circle
-  
+#' Calculate the points on an arc between two points
+#'
+#' @param x0 first point coordinate on x-axis
+#' @param y0 first point coordinate on y-axis
+#' @param x1 second point coordinate on x-axis
+#' @param y1 second point coordinate on y-axis
+#' @param arcdeg degrees of the arc connecting the points (positive is
+#'  counter-clockwise, negative is clockwise)
+#' @param n number of points in resulting arc line
+calcArc <- function(x0 = 1,
+                    y0 = 1,
+                    x1 = 4,
+                    y1 = 6,
+                    arcdeg = 30,
+                    n = 50) {
+
+  if (abs(arcdeg) >- 359.9 & abs(arcdeg) > 359.9) {
+    stop("angle of arc (arcdeg) must be between -359.9 and 359.9")
+  }
+
+  anglerad <- atan2(y = y1 - y0, x = x1 - x0) # angle between points
+  midpt <- list(x = mean(c(x0, x1)), y = mean(c(y0, y1))) # midpoint coordinates
+  # of chord
+  arcrad <- deg2rad(deg = arcdeg) # angle of arc in radian
+  chordlength <- sqrt((x1 - x0)^2 + (y1 - y0)^2) # length between points
+  r <- abs((chordlength / 2) / sin(arcrad / 2)) # radius of circle
+
   # angle from midpoint to circle center
   lut <- data.frame(
-    lessthan180 = c(TRUE, TRUE, FALSE, FALSE), 
-    sign = c(1, -1, 1, -1), 
-    rotation = c(90, -90, -90, 90))
+    lessthan180 = c(TRUE, TRUE, FALSE, FALSE),
+    sign = c(1, -1, 1, -1),
+    rotation = c(90, -90, -90, 90)
+  )
   hit <- which(lut$lessthan180 == (abs(arcdeg) < 180) & lut$sign == sign(arcdeg))
   anglecen <- anglerad + deg2rad(lut$rotation[hit])
-  
+
   # length of midpoint to circle center
-  midpt2cenpt <- sqrt(r^2 - (chordlength/2)^2) 
-  
+  midpt2cenpt <- sqrt(r^2 - (chordlength / 2)^2)
+
   # calculate center point
-  cenpt <- list(x = midpt$x + midpt2cenpt*cos(anglecen), 
-                y = midpt$y + midpt2cenpt*sin(anglecen))
-  
+  cenpt <- list(
+    x = midpt$x + midpt2cenpt*cos(anglecen),
+    y = midpt$y + midpt2cenpt*sin(anglecen)
+  )
+
   # angle from circle center to arc
-  anglecen2arc <- anglecen + ifelse(abs(arcdeg)<180, deg2rad(180), 0)
-  
+  anglecen2arc <- anglecen + ifelse(abs(arcdeg) < 180, deg2rad(180), 0)
+
   # produce vector of arc with n points
   arc <- data.frame(
     rad = seq(
       from = anglecen2arc - arcrad/2,
-      to = anglecen2arc + arcrad/2, 
+      to = anglecen2arc + arcrad/2,
       length.out = n
     )
   )
-  arc$x <- cenpt$x + r*cos(arc$rad)
-  arc$y <- cenpt$y + r*sin(arc$rad)
-  
-  return(arc)
+  arc$x <- cenpt$x + r * cos(arc$rad)
+  arc$y <- cenpt$y + r * sin(arc$rad)
+
+  arc
 }
 
-# function drawing the results of calcArc as a line or arrow.
-# makes a conversion in plotting region units in order to maintain a circular arc
-addArc <- function(x0 = 1, y0 = 1, x1 = 4, y1 = 6, arcdeg = 30, n = 50, 
-                   t = "l", col = 1, lty = 1, lwd = 1, 
-                   arrowlength = NULL, arrowangle = 30, arrowcode = 2,
+#' Draw the results of calcArc as a line or arrow.
+#' makes a conversion in plotting region units to maintain a circular arc
+addArc <- function(x0 = 1,
+                   y0 = 1,
+                   x1 = 4,
+                   y1 = 6,
+                   arcdeg = 30,
+                   n = 50,
+                   t = "l",
+                   col = 1,
+                   lty = 1,
+                   lwd = 1,
+                   arrowlength = NULL,
+                   arrowangle = 30,
+                   arrowcode = 2,
                    result = FALSE,
-                   ...){
-  
+                   ...) {
+
   # calculate arc
   arc <- calcArc(x0 = x0, y0 = y0, x1 = x1, y1 = y1, arcdeg = arcdeg, n = n)
-  
+
   # calculate arc in device units
   FROM = "user"
   TO = "chars"
-  
+
   x0 <- grconvertX(x0, from = FROM, to = TO)
   x1 <- grconvertX(x1, from = FROM, to = TO)
   y0 <- grconvertY(y0, from = FROM, to = TO)
   y1 <- grconvertY(y1, from = FROM, to = TO)
   arc2 <- calcArc(x0 = x0, y0 = y0, x1 = x1, y1 = y1, arcdeg = arcdeg, n = n)
   names(arc2) <- c("rad", "xusr", "yusr")
-  
+
   arc <- cbind(arc, arc2[,c("xusr", "yusr")])
-  
+
   # convert back to user coordinates
   arc$xusr <- grconvertX(arc$xusr, from = TO, to = FROM)
   arc$yusr <- grconvertY(arc$yusr, from = TO, to = FROM)
-  
-  lines(yusr ~ xusr, data = arc, t = t, 
-        col = col, lty = lty, lwd = lwd, ...)
-  if(!is.null(arrowlength)){
-    arrows(x0 = arc$xusr[n-1], x1 = arc$xusr[n], y0 = arc$yusr[n-1], y1 = arc$yusr[n], 
-           length = arrowlength, code = arrowcode, angle = arrowangle, 
-           col = col, lty = lty, lwd = lwd, ...)
+
+  lines(yusr ~ xusr, data = arc, t = t, col = col, lty = lty, lwd = lwd, ...)
+  if (!is.null(arrowlength)) {
+    arrows(
+      x0 = arc$xusr[n-1], x1 = arc$xusr[n], y0 = arc$yusr[n-1], y1 = arc$yusr[n],
+      length = arrowlength, code = arrowcode, angle = arrowangle,
+      col = col, lty = lty, lwd = lwd, ...
+    )
   }
-  
-  if(result){return(arc)}
+
+  if (result == TRUE) { return(arc) }
 }
--- functions/utils.R
+++ functions/utils.R
@@ -1,4 +1,8 @@
-# Define the function to split the dataset into train/test sets
+#' Define the function to split the dataset into train/test sets
+#'
+#' @param data dataset
+#' @param seed seed number to use for replication
+#' @param train_ratio ratio of data to use in the train set (default to 0.7)
 split_dataset <- function(data, seed, train_ratio = 0.7) {
   set.seed(seed)
   train_size <- floor(train_ratio * nrow(data))
@@ -10,11 +14,19 @@ split_dataset <- function(data, seed, train_ratio = 0.7) {
   list(
     data_train = train_set,
     data_test = test_set
-    )
+  )
 }
 
-# Train and predict a logistic regression classifier: aware model
-log_reg_train <- function(train_data, test_data, type = c("aware", "unaware")) {
+#' Train and predict a logistic regression classifier: aware model
+#'
+#' @param train_data train set. The sensitive variable S must be in the data
+#' @param test_data test set. The sensitive variable S must be in the data
+#' @param type type of model to train: if `"aware"`, the sensitive variable S
+#'  is included in the formula of the glm, if `"unaware"`, the sensitive
+#'  variable is not included in the formula of the model.
+log_reg_train <- function(train_data,
+                          test_data,
+                          type = c("aware", "unaware")) {
   if (type == "unaware") {
     train_data_ <- train_data %>% select(-S)
     test_data_ <- test_data %>% select(-S)
@@ -45,25 +57,33 @@ log_reg_train <- function(train_data, test_data, type = c("aware", "unaware")) {
 #' @param h small value added to extend the area covered by the grid (default
 #'  to .2)
 #' @param d neighborhood weight when conditioning by x1 (default to .5)
-fonction_transport <- function(data,
+#'
+#' @returns A list with four elements:
+#' - `Transport_x1`: the function to transport x1 from S=0 to S=1
+#' - `Transport_x2`: the function to transport x2 from S=0 to S=1
+#' - `Transport_x1_cond_x2`: the function to transport x1|x2 from S=0 to S=1
+#' - `Transport_x2_cond_x1`: the function to transport x2|x1 from S=0 to S=1
+#' Note: these functions use appromixated values using a grid. The grid
+#' coordinates are exported with each function.
+transport_function <- function(data,
                                S_0,
                                n_grid = 15,
                                h = .2,
                                d = .5
-                               ) {
+) {
 
-  # Subset of the data: 0 for Black, 1 for White
+  # Subset of the data: S=0, and S=1
   D_SXY_0 <- data[data$S ==S_0, ]
   D_SXY_1 <- data[data$S!= S_0, ]
 
-  # Coordinates of the cells of the grid on subset of 0 (Black)
+  # Coordinates of the cells of the grid on subset of S=0
   vx1_0 <- seq(min(D_SXY_0$X1) - h, max(D_SXY_0$X1) + h, length = n_grid + 1)
   vx2_0 <- seq(min(D_SXY_0$X2) - h, max(D_SXY_0$X2) + h, length = n_grid + 1)
   # and middle point of the cells
   vx1_0_mid <- (vx1_0[2:(1+n_grid)]+vx1_0[1:(n_grid)]) / 2
   vx2_0_mid <- (vx2_0[2:(1+n_grid)]+vx2_0[1:(n_grid)]) / 2
 
-  # Coordinates of the cells of the grid on subset of 1 (White)
+  # Coordinates of the cells of the grid on subset of S=0
   vx1_1 <- seq(min(D_SXY_1$X1) -h, max(D_SXY_1$X1) + h, length = n_grid + 1)
   vx1_1_mid <- (vx1_1[2:(1 + n_grid)] + vx1_1[1:(n_grid)]) / 2
   # and middle point of the cells
@@ -72,39 +92,40 @@ fonction_transport <- function(data,
 
   # Creation of the grids for the CDF and Quantile function
   # init with NA values
-  # One grid for X1 and X2, on both subsets of the data (Black/White)
+  # One grid for X1 and X2, on both subsets of the data (S=0, and S=1)
   F1_0 <- F2_0 <- F1_1 <- F2_1 <- matrix(NA, n_grid, n_grid)
   Q1_0 <- Q2_0 <- Q1_1 <- Q2_1 <- matrix(NA, n_grid, n_grid)
 
-  # Empirical CDF for X1 on subset of Black
+  # Empirical CDF for X1 on subset of S=0
   FdR1_0 <- Vectorize(function(x) mean(D_SXY_0$X1 <= x))
   f1_0 <- FdR1_0(vx1_0_mid)
-  # Empirical CDF for X2 on subset of Black
+  # Empirical CDF for X2 on subset of S=0
   FdR2_0 <- Vectorize(function(x) mean(D_SXY_0$X2 <= x))
   f2_0 <- FdR2_0(vx2_0_mid)
-  # Empirical CDF for X1 on subset of White
+  # Empirical CDF for X1 on subset of S=1
   FdR1_1 <- Vectorize(function(x) mean(D_SXY_1$X1 <= x))
   f1_1 <- FdR1_1(vx1_1_mid)
-  # Empirical CDF for X2 on subset of White
+  # Empirical CDF for X2 on subset of S=1
   FdR2_1 <- Vectorize(function(x) mean(D_SXY_1$X2 <= x))
   f2_1 <- FdR2_1(vx2_1_mid)
 
   u <- (1:n_grid) / (n_grid + 1)
-  # Empirical quantiles for X1 on subset of Black
+  # Empirical quantiles for X1 on subset of S=0
   Qtl1_0 <- Vectorize(function(x) quantile(D_SXY_0$X1, x))
   q1_0 <- Qtl1_0(u)
-  # Empirical quantiles for X2 on subset of Black
+  # Empirical quantiles for X2 on subset of S=0
   Qtl2_0 <- Vectorize(function(x) quantile(D_SXY_0$X2, x))
   q2_0 <- Qtl2_0(u)
-  # Empirical quantiles for X1 on subset of White
+  # Empirical quantiles for X1 on subset of S=1
   Qtl1_1 <- Vectorize(function(x) quantile(D_SXY_1$X1, x))
   q1_1 <- Qtl1_1(u)
-  # Empirical quantiles for X2 on subset of White
+  # Empirical quantiles for X2 on subset of S=1
   Qtl2_1 <- Vectorize(function(x) quantile(D_SXY_1$X2, x))
   q2_1 <- Qtl2_1(u)
 
+  # Compute c.d.f and quantile at each cell of the grid in both groups
   for(i in 1:n_grid) {
-    # Subset of Black
+    # Subset of S=0
     idx1_0 <- which(abs(D_SXY_0$X1 - vx1_0_mid[i]) < d)
     FdR2_0 <- Vectorize(function(x) mean(D_SXY_0$X2[idx1_0] <= x))
     F2_0[, i] <- FdR2_0(vx2_0_mid)
@@ -117,7 +138,7 @@ fonction_transport <- function(data,
     Qtl1_0 <- Vectorize(function(x) quantile(D_SXY_0$X1[idx2_0], x))
     Q1_0[, i] <- Qtl1_0(u)
 
-    # Subset of White
+    # Subset of S=1
     idx1_1 <- which(abs(D_SXY_1$X1 - vx1_1_mid[i]) < d)
     FdR2_1 <- Vectorize(function(x) mean(D_SXY_1$X2[idx1_1] <= x))
     F2_1[, i] <- FdR2_1(vx2_1_mid)
@@ -163,7 +184,7 @@ fonction_transport <- function(data,
   # Transport for X1 conditional on X2
   T1_cond_x2 <- function(x1, x2) {
     k0 <- which.min(abs(vx2_0_mid - x2))
-    k1 <- which.min(abs(vx2_1_mid - x2))
+    k1 <- which.min(abs(vx2_1_mid - T2(x2)))
     i <- which.min(abs(vx1_0_mid - x1))
     p <- F1_0[i, k0]
     i <- which.min(abs(u - p))
